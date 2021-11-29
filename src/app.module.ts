23cec8c8fa1a84e227c2e223a0c0ec727a958a9a
Unchaptered <workstation19961002@gmail.com>
2021-11-29 17:03:33 +0900

    #1.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/* decorator Looks like JSX Components
+  decorator add functions in Class
+ */
+
 @Module({
   imports: [],
   controllers: [AppController],
