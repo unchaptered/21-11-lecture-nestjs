94f1d18b53459733473d519dad07ef3de6c879eb
Unchaptered <workstation19961002@gmail.com>
2021-11-29 17:13:12 +0900

    #1.1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/Hello")
+  sayHello():string {
+    return "Hello everyone";
+  }
 }
+
+/* @ menas "It is Router(express)!!"
+ *  @Get("/url")
+ *  ssayHello << menas Controlelr(express)
+ */
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-/* decorator Looks like JSX Components
-  decorator add functions in Class
+/* @ decorator Looks like JSX Components
+ * @ add functions in Class
+ * @ is positioned next to function
  */
 
 @Module({
@@ -11,4 +12,10 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
+
+/* AppController
+    AooController seems like Express Router-Controlelr(in Routers-Controlelrs)
+    @Get, Post..... (seems like RESTful URl)
+ */
+
 export class AppModule {}
