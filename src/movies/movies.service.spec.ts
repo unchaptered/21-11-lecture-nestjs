bf8686f5af2f98f69e2c6117567d88b0493c3031
Unchaptered <workstation19961002@gmail.com>
2021-11-30 17:32:22 +0900

    #3.1 Your First Unit Test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,14 @@
+// File includes test
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
+
+// Descript What is Test?
 describe('MoviesService', () => {
   let service: MoviesService;
 
+ // Before Testing
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +17,12 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // It or Individual Test
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be 4', ()=>{
+    expect(2+3).toEqual(5)
   });
 });
